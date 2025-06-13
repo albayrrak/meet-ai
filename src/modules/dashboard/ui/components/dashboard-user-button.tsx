ecd64daaf65898d5a3c64367d4839b44a6abc7ca
albayrrak <albayrrak@gmail.com>
2025-06-13 13:49:36 +0300

    sidebar

---
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
+const DashboardUserButton = () => {
+  const { data, isPending } = authClient.useSession();
+
+  if (isPending || !data?.user) {
+    return null;
+  }
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+        {data.user.image ? (
+          <Avatar>
+            <AvatarImage src={data.user.image} />
+          </Avatar>
+        ) : null}
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
+};
+
+export default DashboardUserButton;
