e63a355cb4769cfdaed7f76e2986ffa64a0a5def
albayrrak <albayrrak@gmail.com>
2025-06-17 12:31:58 +0300

    sidebar finish

---
+++ src/modules/dashboard/ui/components/dashboard-navbar.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+const DashboardNavbar = () => {
+  return (
+    <nav className="flex px-4 gap-x-2 items-center py-3 border-b bg-background"></nav>
+  );
+};
+
+export default DashboardNavbar;
